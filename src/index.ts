99e9541c19e992f452d84a057f9235675577a228
Sambo Chea <sombochea100@gmail.com>
2023-09-11 19:40:02 +0700

    Updated

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,16 @@
 import { Elysia } from "elysia";
 
-const app = new Elysia().get("/", () => "Hello Elysia").listen(3000);
+const app = new Elysia()
+  .get("/", () => "ok")
+  .get("/health", () => {
+  return {
+    status: "ok",
+    uptime: process.uptime(),
+    memoryUsage: process.memoryUsage(),
+    version: process.version,
+  };
+})
+.listen(3000);
 
 console.log(
   `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
